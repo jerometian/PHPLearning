736e0334521965028e8a36a8df472b12519afd52
jjtian <jerometian@gmail.com>
2016-01-19 15:55:05 +0800

    first commit for php project

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "²âÊÔ²âÊÔ²âÊÔ¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£"
+?>
+
+<?php
+$cars=array("Volvo","BMW","Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+
+<?php phpinfo() ?>
