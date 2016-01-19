e6664b30679b4f61aa857350dbabdd05470edc63
jjtian <jerometian@gmail.com>
2016-01-19 17:08:42 +0800

    1

--- index.php
+++ index.php
@@ -5,6 +5,36 @@ echo "霞編霞編霞編。。。。。。。。。。。。。"
 <?php
 $cars=array("Volvo","BMW","Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+
+
+
+echo "<BR>";
+
+$x = 5;
+$y = 10;
+function myTest(){
+    global  $x,$y;
+    $y = $x+$y;
+}
+myTest();
+echo $y;
+
+
+echo "<BR>";
+
+function  mytest1()
+{
+    static $x = 0;
+    echo $x ;
+    $x++;
+}
+
+mytest1();
+mytest1();
+mytest1();
 ?>
 
-<?php phpinfo() ?>
+
+
+
