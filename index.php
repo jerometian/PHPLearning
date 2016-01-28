34c993c7878ba09028f45655e9dfd11cd213d199
jjtian <jerometian@gmail.com>
2016-01-28 14:49:44 +0800

    2016 01 28

--- index.php
+++ index.php
@@ -33,6 +33,29 @@ function  mytest1()
 mytest1();
 mytest1();
 mytest1();
+
+print "test print fun";
+
+class Car
+{
+    var $color;
+    function  Car($color="green")
+        {
+            $this->color=$color;
+
+    }
+    function  what_color(){
+        return $this->color;
+    }
+}
+
+echo "<BR>";
+echo strlen("Hello world!");
+echo "<BR>";
+echo strpos("Hello world","world");
+
+define("GREETING","Welcome to W3SCHool.com.cn!");
+echo GREETING;
 ?>
 
 
