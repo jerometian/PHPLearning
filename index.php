5bee3b29e0b8f65b1b5b99f69796309c2f2a5a00
jjtian <jerometian@gmail.com>
2016-01-29 17:18:58 +0800

    0129

--- index.php
+++ index.php
@@ -56,6 +56,68 @@ echo strpos("Hello world","world");
 
 define("GREETING","Welcome to W3SCHool.com.cn!");
 echo GREETING;
+
+
+
+
+echo "<BR>";
+  $x1 = 10;
+  echo $x1 ++;
+  echo "<BR>";
+  echo $x1;
+  echo ++$x1;
+  echo "<BR>";
+  $y1 = 10;
+  echo "<BR>";
+  echo --$y1;
+  echo "<BR>";
+  echo $y1--;
+  echo "<BR>";
+  echo $y1."<BR>";
+  $txt1 = "Hello ";
+  $txt2=$txt1 ."world";
+
+  echo $txt2;
+  $a = "Hello";
+   $b=$a;
+   $b=$a." world!";
+   echo $b;
+$x = "Hello";
+$x.=" world!";
+
+echo $x;
+
+
+$x=100;
+$y = "100";
+var_dump($x==$y);
+echo "<BR>";
+var_dump($x===$y);
+echo "<BR>";
+var_dump($x!=$y);
+echo "<BR>";
+var_dump($x!==$y);
+echo "<BR>";
+
+$a=50;
+$b=90;
+
+var_dump($a > $b);
+echo "<br>";
+var_dump($a < $b);
+echo "<br>";
+$x = array("a"=>"red","b"=>"gree");
+$y= array("c"=>"blue","d"=>"yellow");
+var_dump($x);
+$z=$x+$y;
+var_dump($z);
+echo "<BR>";
+
+foreach ($z as $k=>$v){
+    //echo $k,':',$v,'<BR>';
+    echo "$z[$k]=$v","<BR>";
+}
+
 ?>
 
 
